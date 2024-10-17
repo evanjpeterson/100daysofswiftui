8f7f08bfb2420fb2ef92df58c0cfbf625b9daf4c
Evan Peterson <evan@evanjp.com>
2024-10-17 16:55:44 +0200

    Finishing WordScramble. Styling it. Pulling some hair out.

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -18,6 +18,12 @@ struct ContentView: View {
     @State private var errorMessage = ""
     @State private var showingError = false
 
+    func resetGame() {
+        usedWords = []
+        newWord = ""
+        startGame()
+    }
+
     func startGame() {
         if let wordsFileURL = Bundle.main.url(
             forResource: "words",
@@ -63,6 +69,14 @@ struct ContentView: View {
         return misspelledRange.location == NSNotFound
     }
 
+    func wordIsNotRoot(_ word: String) -> Bool {
+        return word != rootWord
+    }
+
+    func wordIsLongEnough(_ word: String) -> Bool {
+        return word.count >= 3
+    }
+
     func wordError(title: String, msg: String) {
         errorTitle = title
         errorMessage = msg
@@ -95,6 +109,20 @@ struct ContentView: View {
             )
             return
         }
+        guard wordIsNotRoot(answer) else {
+            wordError(
+                title: "Try again!",
+                msg: "You're not wrong, but you get no points!"
+            )
+            return
+        }
+        guard wordIsLongEnough(answer) else {
+            wordError(
+                title: "Try again!",
+                msg: "Enter a word that's at least 3 letters long."
+            )
+            return
+        }
 
         withAnimation {
             usedWords.insert(answer, at: 0)
@@ -103,33 +131,97 @@ struct ContentView: View {
         hasFocus = true
     }
 
+    func getScore() -> Double {
+        let letterMultiplier = 1 +
+            Double(usedWords.reduce(0) { $0 + $1.count }) * 0.01
+        let rawScore = Double(usedWords.count) * letterMultiplier
+        return round(rawScore * 1000) / 1000
+    }
+
     var body: some View {
         NavigationStack {
             List {
+                Group {
+                    Text("Use the letters in ") +
+                        Text("\n\(rootWord)").font(.largeTitle) +
+                        Text("\nto create new words.")
+                }
+                .font(.title3)
+                .listRowBackground(Color.white.opacity(0))
+                .foregroundStyle(.white)
                 Section {
-                    TextField("Enter your word", text: $newWord)
-                        .textInputAutocapitalization(.never)
-                        .autocorrectionDisabled()
-                        .focused($hasFocus)
+                    TextField(
+                        "Test",
+                        text: $newWord,
+                        prompt: Text("Enter your word").foregroundStyle(.gray)
+                    )
+                    .textInputAutocapitalization(.never)
+                    .autocorrectionDisabled()
+                    .focused($hasFocus)
+                    .foregroundStyle(.white)
                 }.onSubmit(addNewWord)
+                    .font(.title2)
+                    .padding(8)
+                    .listRowBackground(Color("FieldBG"))
+                if usedWords.count > 0 {
+                    Section {
+                        Group {
+                            Text("Score: \(String(format: "%.2f", getScore()))")
+                                .font(.title)
+                                +
+                                Text(
+                                    "\nFormula: <# words> * (1 + <# letters> / 100)"
+                                )
+                                .font(.footnote)
+                        }
+                    }
+                    .listRowBackground(Color("FieldBG"))
+                    .foregroundStyle(.white)
+                }
                 Section {
-                    ForEach(usedWords, id: \.self) { word in
+                    ForEach(Array(usedWords.enumerated()),
+                            id: \.element)
+                    { i, word in
                         HStack {
                             Image(systemName: "\(word.count).circle")
                             Text(word)
+                            Spacer()
+                            Text("\(usedWords.count - i)")
                         }
+                        .foregroundStyle(.white)
+                        .listRowBackground(Color("FieldBG"))
                     }
                 }
-            }.navigationTitle(rootWord)
-                .onAppear(perform: startGame)
-                .alert(errorTitle, isPresented: $showingError) {
-                    Button("Shucks, OK") {
-                        newWord = ""
-                        hasFocus = true
-                    }
-                } message: {
-                    Text(errorMessage)
+            }
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("Shucks, OK") {
+                    newWord = ""
+                    hasFocus = true
                 }
+            } message: {
+                Text(errorMessage)
+            }
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("WordScramble")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                }
+                /*
+                 ToolbarItem(placement: .topBarTrailing) {
+                     Button {
+                         resetGame()
+                     } {
+                         Text("Restart").foregroundStyle(.white)
+                     }
+                 }
+                  */
+            }
+            .toolbarBackground(Color("AppBG").opacity(0.9), for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
+            .scrollContentBackground(.hidden)
+            .background(Color("AppBG"))
         }
     }
 }
